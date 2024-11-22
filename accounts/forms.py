b3cea3222f8624355de0e9b158c3198d4ec6575a
siam1299 <146884292+siam1299@users.noreply.github.com>
2024-11-22 17:08:49 +0600

    user login & user profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from django.contrib.auth.models import User
 
@@ -7,3 +7,8 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.TextInput()
+    widget = forms.PasswordInput()
