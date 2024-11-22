8957009f3f504968fb66a4edd6d39dbe4b0d9de7
Tanmoy Ranjan Dhar <antarswapno80@gmail.com>
2024-11-22 21:28:12 +0600

    add new book

--- books/urls.py
+++ books/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("books", views.book_list, name="book_list"),
+    path("books/add", views.add_book, name="add_book"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, redirect
+from .forms import BookForm
 from .models import Book
+
+
+# Create your views here.
+def add_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = BookForm()
+    return render(request, "books/book_form.html", {"form": form})
 
 
 def book_list(request):
