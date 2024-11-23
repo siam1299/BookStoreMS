4e02e9ed534f31a6873232ac6ab79063b90c068d
Zaff <22101037@uap-bd.edu>
2024-11-23 19:12:24 +0600

    created shopping cart page

--- customers/admin.py
+++ customers/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, Cart, CartItem
 
 
 # Register your models here.
 admin.site.register(Customer)
+admin.site.register(Cart)
+admin.site.register(CartItem)
--- customers/models.py
+++ customers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from books.models import Book
 
 
 # Create your models here.
@@ -10,3 +11,21 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Cart(models.Model):
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, related_name="cart")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total_items = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Cart for {self.customer.user.username}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} items in cart"
