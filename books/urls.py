fe3b6c02e9462c00bf920d372368e83487750c59
Tanmoy Ranjan Dhar <antarswapno80@gmail.com>
2024-11-25 00:58:51 +0600

    book details page

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("books", views.book_list, name="book_list"),
     path("books/add", views.add_book, name="add_book"),
+    path("books/<int:pk>", views.book_detail, name="book_detail"),
 ]
--- books/views.py
+++ books/views.py
@@ -18,3 +18,8 @@ def add_book(request):
 def book_list(request):
     books = Book.objects.all()
     return render(request, "books/book_list.html", {"books": books})
+
+
+def book_detail(request, pk):
+    book = Book.objects.get(pk=pk)
+    return render(request, "books/book_detail.html", {"book": book})
